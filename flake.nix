26ec927b2ad79af3aa16143894f2147a644dbe47
forest lehrman <hiracat@proton.me>
2024-06-09 22:17:08 -0400

    added home-manager

--- flake.nix
+++ flake.nix
@@ -4,17 +4,28 @@
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager/master";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = { self, nixpkgs, ...}:
+    outputs = { self, nixpkgs, home-manager, ...}:
         let
+            system = "x86_64-linux";
             lib = nixpkgs.lib;
+            pkgs = nixpkgs.legacyPackages.${system};
         in {
 
         nixosConfigurations = {
             nixos = lib.nixosSystem {
-                system = "x86_64-linux";
+                inherit system;
                 modules = [ ./configuration.nix ];
+            };
+        };
+
+        homeConfigurations = {
+            forest = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [ ./home.nix ];
             };
         };
     };
