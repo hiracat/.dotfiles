8d5f744d10fb0a0f20ce598705b8703d05d9ecb8
forest lehrman <hiracat@proton.me>
2024-06-10 12:12:55 -0400

    added git configuration

--- home.nix
+++ home.nix
@@ -127,6 +127,15 @@ in
     '';
   };
 
+  programs.git = {
+      enable = true;
+      userName = userSettings.username;
+      userEmail = userSettings.email;
+      extraConfig = {
+          init.defaultBranch = "main";
+      };
+  };
+
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
