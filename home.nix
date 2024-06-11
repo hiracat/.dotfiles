7b24e446ae9b2afa3a04af8f4790a58bc2cd3c8c
forest <hiracat@proton.me>
2024-06-10 20:37:32 -0400

    changed indentation

--- home.nix
+++ home.nix
@@ -1,142 +1,140 @@
 { config, pkgs, pkgs-stable, systemSettings, userSettings, ... }:
 
 let
-  myAliases = {
-    ll = "ls -l";
-    ".." = "cd ..";
-    nv = "nvim";
-  };
+    myAliases = {
+        ll = "ls -l";
+        ".." = "cd ..";
+        nv = "nvim";
+    };
 in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = userSettings.username;
-  home.homeDirectory = "/home/${userSettings.username}";
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = userSettings.username;
+    home.homeDirectory = "/home/${userSettings.username}";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    home.packages = [
+        # # Adds the 'hello' command to your environment. It prints a friendly
+        # # "Hello, world!" when run.
+        # pkgs.hello
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+        # # It is sometimes useful to fine-tune packages, for example, by applying
+        # # overrides. You can do that directly here, just don't forget the
+        # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+        # # fonts?
+        # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+        # # You can also create simple shell scripts directly inside your
+        # # configuration. For example, this adds a command 'my-hello' to your
+        # # environment:
+        # (pkgs.writeShellScriptBin "my-hello" ''
+        #   echo "Hello, ${config.home.username}!"
+        # '')
+    ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    home.file = {
+        # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+        # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+        # # symlink to the Nix store copy.
+        # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/forest/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    GTK_IM_MODULE="fcitx";
-    QT_IM_MODULE="fcitx";
-    SDL_IM_MODULE="fcitx";
-    XMODIFIERS="@im=fcitx";
-    QT_QPA_PLATFORMTHEME="qt5ct";
-
-    EDITOR = "nvim";
-    TERMINAL="kitty";
-
-  };
-
-  programs.bash = {
-    enable = true;
-    shellAliases = myAliases;
-  };
-
-  programs.zsh = {
-    enable = true;
-    shellAliases = myAliases;
-    autocd = true;
-    defaultKeymap = "emacs";
-    enableCompletion = true;
-    history = {
-        save = 10000;
-        size = 10000;
-        path = "${config.home.homeDirectory}/.histfile";
+        # # You can also set the file content immediately.
+        # ".gradle/gradle.properties".text = ''
+        #   org.gradle.console=verbose
+        #   org.gradle.daemon.idletimeout=3600000
+        # '';
     };
 
-    initExtra = ''
-      eval "$(starship init zsh)"
-      eval "$(atuin init zsh)"
-      clear
-      fastfetch
-    '';
-  };
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/forest/etc/profile.d/hm-session-vars.sh
+    #
+    home.sessionVariables = {
+        GTK_IM_MODULE="fcitx";
+        QT_IM_MODULE="fcitx";
+        SDL_IM_MODULE="fcitx";
+        XMODIFIERS="@im=fcitx";
+        QT_QPA_PLATFORMTHEME="qt5ct";
 
-  programs.kitty = {
-    enable = true;
-    shellIntegration = {
-      mode = "no-rc"; # disable features if desired here
-      enableZshIntegration = true;
-      enableBashIntegration = true;
+        EDITOR = "nvim";
+        TERMINAL="kitty";
     };
 
-    theme = "Catppuccin-Mocha";
-    font.name = userSettings.font;
-    font.size = 12;
-    font.package = userSettings.fontPkg;
-    extraConfig = ''
-    '';
-  };
+    programs.bash = {
+        enable = true;
+        shellAliases = myAliases;
+    };
 
-  programs.git = {
-      enable = true;
-      userName = userSettings.username;
-      userEmail = userSettings.email;
-      extraConfig = {
-          init.defaultBranch = "main";
-      };
-  };
+    programs.zsh = {
+        enable = true;
+        shellAliases = myAliases;
+        autocd = true;
+        defaultKeymap = "emacs";
+        enableCompletion = true;
+        history = {
+            save = 10000;
+            size = 10000;
+            path = "${config.home.homeDirectory}/.histfile";
+        };
 
+        initExtra = ''
+            eval "$(starship init zsh)"
+            eval "$(atuin init zsh)"
+            clear
+            fastfetch
+        '';
+    };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    programs.kitty = {
+        enable = true;
+        shellIntegration = {
+            mode = "no-rc"; # disable features if desired here
+            enableZshIntegration = true;
+            enableBashIntegration = true;
+        };
+
+        theme = "Catppuccin-Mocha";
+        font.name = userSettings.font;
+        font.size = 12;
+        font.package = userSettings.fontPkg;
+        extraConfig = ''
+        '';
+    };
+
+    programs.git = {
+        enable = true;
+        userName = userSettings.username;
+        userEmail = userSettings.email;
+        extraConfig = {
+            init.defaultBranch = "main";
+        };
+    };
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
 }
