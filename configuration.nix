c58c0afd877d8d651f902c49cbb30a3d0178fd10
forest <hiracat@proton.me>
2024-06-11 16:37:13 -0400

    added caps2escape and interception-tools

--- configuration.nix
+++ configuration.nix
@@ -132,6 +132,18 @@
     openFirewall = true;
   };
 
+  services.interception-tools = {
+      enable = true;
+      plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+      udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+      '';
+  };
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
