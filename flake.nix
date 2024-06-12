096665522c09b23c482c80e519e6d8d175687dbe
forest <hiracat@proton.me>
2024-06-12 16:00:04 -0400

    formatted flake.nix

--- flake.nix
+++ flake.nix
@@ -1,58 +1,60 @@
 {
-    description = "hiracat's dotfiles flake";
+  description = "hiracat's dotfiles flake";
 
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-unstable";
-        nixpkgs-stable.url = "nixpkgs/nixos-24.05";
-        home-manager.url = "github:nix-community/home-manager/master";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ...}:
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }:
 
     let
-        lib = nixpkgs.lib;
-        pkgs = nixpkgs.legacyPackages.${systemSettings.system};
-        pkgs-stable = import nixpkgs-stable {
-            config.allowUnfree = true;
-            stable = nixpkgs-stable.legacyPackages.${systemSettings.system};
-            system = systemSettings.system;
-        };
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${systemSettings.system};
+      pkgs-stable = import nixpkgs-stable {
+        config.allowUnfree = true;
+        stable = nixpkgs-stable.legacyPackages.${systemSettings.system};
+        system = systemSettings.system;
+      };
 
-        systemSettings = {
-            system = "x86_64-linux";
-            hostname = "nixos";
-            timezone = "America/New_York";
-            locale = "en_US.UTF-8";
-        };
+      systemSettings = {
+        system = "x86_64-linux";
+        hostname = "nixos";
+        timezone = "America/New_York";
+        locale = "en_US.UTF-8";
+      };
 
-        userSettings = {
-            username = "forest";
-            email = "hiracat@proton.me";
-            editor = "nvim";
-            terminal = "kitty";
-            font = "JetBrainsMono Nerd Font";
-            fontPkg = pkgs.nerdfonts;
-        };
+      userSettings = {
+        username = "forest";
+        email = "hiracat@proton.me";
+        editor = "nvim";
+        terminal = "kitty";
+        font = "JetBrainsMono Nerd Font";
+        fontPkg = pkgs.nerdfonts;
+      };
 
-    in {
-       nixosConfigurations = {
-           nixos = lib.nixosSystem {
-               system = systemSettings.system;
-               specialArgs = { inherit systemSettings userSettings pkgs-stable; };
-               modules = [
-                    ./configuration.nix
-                    home-manager.nixosModules.home-manager {
-                        home-manager.extraSpecialArgs = {inherit systemSettings userSettings pkgs-stable; };
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.backupFileExtension = "backup";
-                        home-manager.users.${userSettings.username} = {
-                            imports = [ ./home.nix ];
-                        };
-                    }
-               ];
-           };
-       };
+    in
+    {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          system = systemSettings.system;
+          specialArgs = { inherit systemSettings userSettings pkgs-stable; };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = { inherit systemSettings userSettings pkgs-stable; };
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.${userSettings.username} = {
+                imports = [ ./home.nix ];
+              };
+            }
+          ];
+        };
+      };
     };
 }
