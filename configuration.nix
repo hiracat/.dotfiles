fd9a4903def6158bf158863715bd1009e0f82373
forest <hiracat@proton.me>
2024-06-23 18:49:31 -0400

    added media control keys and alsa stuff

--- configuration.nix
+++ configuration.nix
@@ -45,11 +45,12 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  #services.desktopManager.plasma6.enable = true;
 
 
 
@@ -72,6 +73,7 @@
   hardware.pulseaudio.enable = false;
 
   security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -83,6 +85,14 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+  };
+
+  sound = {
+    enable = true;
+    mediaKeys = {
+      enable = true;
+      volumeStep = "2";
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
