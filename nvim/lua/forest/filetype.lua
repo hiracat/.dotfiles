aa7cc37a2132049d72c0b192b38a8dc1dfdb5616
forest <hiracat@proton.me>
2024-07-10 16:27:53 -0400

    fixed indentation, added syntax highlighting for certain config fliles

---
+++ nvim/lua/forest/filetype.lua
@@ -0,0 +1,9 @@
+vim.filetype.add({
+	extension = { rasi = "rasi" },
+	pattern = {
+		[".*/waybar/config"] = "jsonc",
+		[".*/mako/config"] = "dosini",
+		[".*/kitty/*.conf"] = "bash",
+		[".*/hypr/.*%.conf"] = "hyprlang",
+	},
+})
--- nvim/lua/forest/init.lua
+++ nvim/lua/forest/init.lua
@@ -1,3 +1,4 @@
 require("forest.lazy")
 require("forest.remap")
 require("forest.set")
+require("forest.filetype")
