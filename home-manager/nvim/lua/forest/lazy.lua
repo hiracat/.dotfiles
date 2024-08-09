a6008f46f8c986bf5cfc80d45c63c8fe2b9aeee5
forest <hiracat@proton.me>
2024-08-08 23:22:14 -0400

    updated nvim telescope

--- home-manager/nvim/lua/forest/lazy.lua
+++ home-manager/nvim/lua/forest/lazy.lua
@@ -90,9 +90,10 @@ local plugins = {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
+
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
+		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 
