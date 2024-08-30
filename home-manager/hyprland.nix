43c0efc39e4b7a838372767a5e527ae8499d6942
forest <hiracat@proton.me>
2024-08-29 21:54:29 -0400

    fixed the service by adding it to configuration.nix and not hyprland.nix which is in homemanager

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -1,19 +1,5 @@
 { pkgs, ... }:
 {
-  user.systemd.extraServices.polkit_gnome_agent = {
-    description = "gnome polkit graphical authentication agent";
-    wantedBy = [ "hyprland-session.target" ];
-    wants = [ "hyprland-session.target" ];
-    after = [ "hyprland-session.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
-
   wayland.windowManager.hyprland = {
     systemd.enable = true;
     enable = true;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,6 +13,21 @@
 
   systemd.coredump.enable = false; # make core dumps appear in the working directory
 
+  systemd.user.services.polkit_gnome_agent = {
+    description = "gnome polkit graphical authentication agent";
+    wantedBy = [ "hyprland-session.target" ];
+    wants = [ "hyprland-session.target" ];
+    after = [ "hyprland-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+
   networking = {
     networkmanager.enable = true;
     firewall.enable = false;
