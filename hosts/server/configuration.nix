01f8088e532b60a461e7eab8ffe8e8ef2bb55933
forest <hiracat@proton.me>
2024-12-21 23:38:19 -0500

    more synchting

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -19,4 +19,58 @@
   };
   periferals.drawingTablet.enable = true;
 
+  services = {
+    syncthing = {
+      enable = true;
+      group = "users";
+      user = settings.username;
+      dataDir = "/home/${settings.username}/Syncthing";
+      configDir = "/home/${settings.username}/.config/syncthing";
+      overrideDevices = true; # overrides any devices added or deleted through the WebUI
+      overrideFolders = true; # overrides any folders added or deleted through the WebUI
+      settings = {
+        devices = {
+          "desktop" = { id = "FU5NOIY-RG6LIJ3-VWA64FP-6CEZXOP-KZEGB7S-7L3ET5Y-4ITXCDY-YTXXGA4"; };
+        };
+        folders = {
+          "Desktop" = {
+            path = "/home/${settings.username}/Desktop";
+            devices = [ "desktop" ];
+            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+          };
+          "Documents" = {
+            path = "/home/${settings.username}/Documents";
+            devices = [ "desktop" ];
+            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+          };
+          "Downloads" = {
+            path = "/home/${settings.username}/Downloads";
+            devices = [ "desktop" ];
+            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+          };
+          "Music" = {
+            path = "/home/${settings.username}/Music";
+            devices = [ "desktop" ];
+            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+          };
+          "Pictures" = {
+            path = "/home/${settings.username}/Pictures";
+            devices = [ "desktop" ];
+            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+          };
+          "Videos" = {
+            path = "/home/${settings.username}/Videos";
+            devices = [ "desktop" ];
+            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+          };
+          ".dotfiles" = {
+            path = "/home/${settings.username}/.dotfiles";
+            devices = [ "desktop" ];
+            ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+          };
+        };
+      };
+    };
+  };
+
 }
