c645f2b735f9bf56dbc7b9c754bec5f9f4759e84
forest <hiracat@proton.me>
2025-01-09 20:49:33 -0500

    external drive automount, start steam on boot

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -20,6 +20,18 @@
 
   periferals.drawingTablet.enable = true;
 
+  fileSystems."/run/media/forest/backups" = {
+    device = "/dev/disk/by-uuid/26975e28-ef0a-4681-8e45-5c0af5da170a";
+    fsType = "ext4";
+    options = [
+      # If you don't have this options attribute, it'll default to "defaults"
+      # boot options for fstab. Search up fstab mount options you can use
+      "users" # Allows any user to mount and unmount
+      "nofail" # Prevent system from failing if this drive doesn't mount
+      "x-gvfs-show" # snow drive in fileexplorer
+    ];
+  };
+
   services = {
     syncthing = {
       enable = true;
