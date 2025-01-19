0457a203b34393e9fe10d8e521178d559b19a55d
forest <hiracat@proton.me>
2025-01-19 18:21:51 -0500

    setup ssh login with gh-cli

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -6,9 +6,10 @@
     userEmail = "hiracat@proton.me";
     extraConfig = {
       init.defaultBranch = "main";
-      credential.helper = "store";
       push.autoSetupRemote = true;
       push.default = "simple";
+      credential."https://github.com".helper = "!/run/current-system/sw/bin/gh auth git-credential";
+      credential."https://gist.github.com".helper = "!/run/current-system/sw/bin/gh auth git-credential";
     };
   };
 }
