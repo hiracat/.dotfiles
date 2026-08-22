4e5cf10ffc9b6b9d0c44531a799b36130d6f3762
hiracat <hiracat@proton.me>
2026-08-22 19:33:54 -0400

    some changes

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,5 +1,4 @@
 { ... }: {
-
   programs.git = {
     enable = true;
     settings = {
@@ -10,7 +9,7 @@
       push.default = "simple";
       credential."https://github.com".helper = "!/run/current-system/sw/bin/gh auth git-credential";
       credential."https://gist.github.com".helper = "!/run/current-system/sw/bin/gh auth git-credential";
-      aliases = {
+      alias = {
         lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
       };
     };
